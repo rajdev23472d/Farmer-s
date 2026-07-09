35fc392bfbbe6c39b8aa2b9d4406762b4850d94c
rajdev23472d <rajdev23472@gmail.com>
2026-07-09 17:34:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".hero button");
+
+  button.addEventListener("click", function () {
+    alert("🌾 Welcome to Farmer Mitra App!");
+  });
+});
